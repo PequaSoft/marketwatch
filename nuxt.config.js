4bcc9d52cb059fba0a3b30d688ed9007894d1be4
Frank <flagumina@yahoo.com>
2020-07-19 22:48:35 -0400

    commit18

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,88 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
-  mode: 'spa',
+  mode: 'universal',
   /*
-   ** Headers of the page
-   */
+  ** Headers of the page
+  */
   head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
   /*
-   ** Customize the progress-bar color
-   */
+  ** Customize the progress-bar color
+  */
   loading: { color: '#fff' },
   /*
-   ** Global CSS
-   */
-  css: [],
+  ** Global CSS
+  */
+  css: [
+  ],
   /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/bus',
+    { src: "@/plugins/chart", ssr: false } //,
+    //{ src: '~/plugins/vue-timers', mode: 'client' }
+  ],
+  
   /*
-   ** Nuxt.js modules
-   */
-  modules: [],
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify'
+  ],
   /*
-   ** Build configuration
-   */
+  ** Nuxt.js modules
+  */
+  modules: ["cookie-universal-nuxt"],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        light: {
+          primary: colors.blue.darken2,
+          accent: colors.blue.darken4,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
   build: {
     /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {},
-  },
-};
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
